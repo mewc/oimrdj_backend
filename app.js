311063dcd42243f5b38b6bfe887e2b118953ef7d
Unknown <mikecarter771@gmail.com>
2018-09-17 17:03:57 +1000

    search query returns full results
    to shrink the return results file

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/spotifyToken', indexRouter);
+app.use('/search', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -29,4 +29,35 @@ router.get('/spotifytoken', function(req, res, next) {
 
 });
 
+/* GET home page. */
+router.get('/search', function(req, res, next) {
+
+    let spotifyApi = new SpotifyWebApi({
+        clientId: '0418232de85048ae9be415d18c75202a',
+        clientSecret: 'a531ce08054d49cd8123a1cf8d46ba10',
+    });
+    let query = req.query.query;
+    console.log(req.query.query);
+    console.log(res);
+
+    spotifyApi.clientCredentialsGrant().then(
+        function(data) {
+            console.log(data);
+            spotifyApi.setAccessToken(data.body['access_token']);
+
+            spotifyApi.searchTracks(query)
+                .then(function(data) {
+                    console.log(data);
+                    res.status(200).json(data)
+                },function(err){
+                    console.log(err);
+                })
+        },
+        function(err){
+            console.log(err);
+        })
+
+
+});
+
 module.exports = router;
