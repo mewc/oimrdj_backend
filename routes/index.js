d1f79074c756a243d70889d7624fd2955e1d4bba
Unknown <mikecarter771@gmail.com>
2018-09-17 15:25:37 +1000

    return spotify token

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var SpotifyWebApi = require('spotify-web-api-node');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +9,23 @@ router.get('/', function(req, res, next) {
 
 /* GET home page. */
 router.get('/spotifytoken', function(req, res, next) {
-    res.render('index', { title: 'Spotify token' });
+    // res.render('index', { title: 'Spotify token' });
+
+    let spotifyApi = new SpotifyWebApi({
+        clientId: '0418232de85048ae9be415d18c75202a',
+        clientSecret: 'a531ce08054d49cd8123a1cf8d46ba10',
+    });
+
+    spotifyApi.clientCredentialsGrant().then(
+        function(data) {
+        console.log(data);
+        spotifyApi.setAccessToken(data.body['access_token']);
+        res.status(200).json({spotifyAccessToken: data.body['access_token']})
+
+        },
+    function(err){
+            console.log(err);
+    })
 
 });
 
