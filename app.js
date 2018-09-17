52367a786635e78ba5d346799eeec55f840b0212
Unknown <mikecarter771@gmail.com>
2018-09-17 16:26:31 +1000

    fix typo, rm doubleup

--- app.js
+++ app.js
@@ -14,19 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-//cors
-// Website you wish to allow to connect
-res.setHeader('Access-Control-Allow-Origin', '*');
-
-// Request methods you wish to allow
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-// Request headers you wish to allow
-res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-// Set to true if you need the website to include cookies in the requests sent
-// to the API (e.g. in case you use sessions)
-res.setHeader('Access-Control-Allow-Credentials', true);
 
 
 
@@ -35,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.user(cors());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/spotifyToken', indexRouter);
